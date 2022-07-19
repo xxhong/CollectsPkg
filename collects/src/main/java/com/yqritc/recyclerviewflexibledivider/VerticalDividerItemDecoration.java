30b0a98688f02ddd7199eff1c3892e074ff61f0c
Houn <xuxihong@minijoy.work>
2022-07-20 02:15:07 +0800

    fix

--- collects/src/main/java/com/yqritc/recyclerviewflexibledivider/VerticalDividerItemDecoration.java
+++ collects/src/main/java/com/yqritc/recyclerviewflexibledivider/VerticalDividerItemDecoration.java
@@ -3,10 +3,11 @@ package com.yqritc.recyclerviewflexibledivider;
 import android.content.Context;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
-import android.support.annotation.DimenRes;
-import android.support.v4.view.ViewCompat;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
+
+import androidx.annotation.DimenRes;
+import androidx.core.view.ViewCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 /**
  * Created by yqritc on 2015/01/15.
