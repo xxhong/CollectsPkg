ba5356380f1d67b20bc7412d14516b6f07d8a7eb
Houn <xuxihong@minijoy.work>
2022-07-21 22:49:11 +0800

    add tag

---
+++ collects/src/main/java/me/gujun/android/taggroup/TagKV.java
@@ -0,0 +1,11 @@
+package me.gujun.android.taggroup;
+
+public class TagKV {
+    public String key;
+    public String value;
+
+    public TagKV(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+}
