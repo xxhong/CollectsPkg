31c977bbfee038973ccf9c462d02783625716f01
Houn <xuxihong@minijoy.work>
2024-09-10 20:48:33 +0800

    change pkg name

---
+++ app/src/main/java/network/minijoy/collectspkg/MainActivity.kt
@@ -0,0 +1,12 @@
+package network.minijoy.collectspkg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import es.dmoral.toastysample.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
